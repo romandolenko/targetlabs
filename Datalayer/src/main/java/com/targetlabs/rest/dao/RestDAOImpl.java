6321da87d0a4de815e234394a35c877506215685
Roman Dolenko <roman.dolenko@olfatrade.com>
2017-08-15 18:46:48 +0300

    Add structure

--- Datalayer/src/main/java/com/targetlabs/rest/dao/RestDAOImpl.java
+++ Datalayer/src/main/java/com/targetlabs/rest/dao/RestDAOImpl.java
@@ -1,7 +1,16 @@
 package com.targetlabs.rest.dao;
 
+import org.springframework.stereotype.Repository;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+
 /**
  * @author Dolenko Roman <dolenko.roman@gmail.com> on 13.08.2017.
  */
+@Repository
 public class RestDAOImpl implements RestDAO {
+
+    @PersistenceContext
+    private EntityManager entityManager;
 }
