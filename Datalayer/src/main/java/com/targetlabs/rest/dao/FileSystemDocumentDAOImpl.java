6955e5eaebb43ffec3d9c223f865ef36e202f10f
Roman <dolenko.roman@gmail.com>
2017-08-21 05:02:53 +0300

    Implementing services

--- Datalayer/src/main/java/com/targetlabs/rest/dao/FileSystemDocumentDAOImpl.java
+++ Datalayer/src/main/java/com/targetlabs/rest/dao/FileSystemDocumentDAOImpl.java
@@ -12,7 +12,6 @@ import java.nio.file.Paths;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.*;
-import java.util.stream.Collectors;
 
 /**
  * FileSystemDocumentDAO saves documents and meta data in the file system.
@@ -39,9 +38,9 @@ public class FileSystemDocumentDAOImpl implements FileSystemDocumentDAO {
      * @param document Document
      * @return The id of the document
      * @throws IOException if the file does not exist,
-     *                   is a directory rather than a regular file,
-     *                   or for some other reason cannot be opened for
-     *                   saving.
+     *                     is a directory rather than a regular file,
+     *                     or for some other reason cannot be opened for
+     *                     saving.
      */
     @Override
     public String saveDocument(Document document) throws IOException {
@@ -65,9 +64,9 @@ public class FileSystemDocumentDAOImpl implements FileSystemDocumentDAO {
      *
      * @param metadataDocument MetadataDocument
      * @throws IOException if the file does not exist,
-     *                   is a directory rather than a regular file,
-     *                   or for some other reason cannot be opened for
-     *                   saving.
+     *                     is a directory rather than a regular file,
+     *                     or for some other reason cannot be opened for
+     *                     saving.
      */
     @Override
     public void saveMetadataDocument(MetadataDocument metadataDocument) throws IOException {
@@ -82,13 +81,13 @@ public class FileSystemDocumentDAOImpl implements FileSystemDocumentDAO {
      * Finds meta data documents by parameters.
      * Parameters are not mandatory, to find all meta data all params should be null.
      *
-     * @param userName User name
+     * @param userName     User name
      * @param localization document localization
      * @return List of meta data documents.
-     * @throws IOException if the file does not exist,
-     *                   is a directory rather than a regular file,
-     *                   or for some other reason cannot be opened for
-     *                   reading.
+     * @throws IOException    if the file does not exist,
+     *                        is a directory rather than a regular file,
+     *                        or for some other reason cannot be opened for
+     *                        reading.
      * @throws ParseException if property file can not be parsed.
      */
     @Override
@@ -111,10 +110,10 @@ public class FileSystemDocumentDAOImpl implements FileSystemDocumentDAO {
      * Finds all meta data documents.
      *
      * @return List of meta data documents.
-     * @throws IOException if the file does not exist,
-     *                   is a directory rather than a regular file,
-     *                   or for some other reason cannot be opened for
-     *                   reading.
+     * @throws IOException    if the file does not exist,
+     *                        is a directory rather than a regular file,
+     *                        or for some other reason cannot be opened for
+     *                        reading.
      * @throws ParseException if property file can not be parsed.
      */
     @Override
@@ -129,10 +128,10 @@ public class FileSystemDocumentDAOImpl implements FileSystemDocumentDAO {
      *
      * @param id The id of the document
      * @return A document incl. file and meta data
-     * @throws IOException if the file does not exist,
-     *                   is a directory rather than a regular file,
-     *                   or for some other reason cannot be opened for
-     *                   reading.
+     * @throws IOException    if the file does not exist,
+     *                        is a directory rather than a regular file,
+     *                        or for some other reason cannot be opened for
+     *                        reading.
      * @throws ParseException if property file can not be parsed.
      */
     @Override
--- Service/src/test/java/com/targetlabs/rest/service/RestServiceFacadeTest.java
+++ Service/src/test/java/com/targetlabs/rest/service/RestServiceFacadeTest.java
@@ -1,20 +1,43 @@
 package com.targetlabs.rest.service;
 
 import com.targetlabs.rest.config.Application;
+import com.targetlabs.rest.config.MailConfiguration;
+import com.targetlabs.rest.dao.FileSystemDocumentDAOImpl;
+import com.targetlabs.rest.facade.DocumentServiceController;
+import com.targetlabs.rest.notification.NotificationService;
+import com.targetlabs.rest.protocol.MetadataDocument;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+import org.springframework.mock.web.MockMultipartFile;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.test.util.ReflectionTestUtils;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+
+import java.io.File;
+import java.util.Date;
+import java.util.List;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.hamcrest.Matchers.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 /**
  * @author Dolenko Roman <dolenko.roman@gmail.com> on 20.08.2017.
  */
 @RunWith(SpringJUnit4ClassRunner.class)
-@ContextConfiguration(classes = {Application.class})
+@ContextConfiguration(classes = {Application.class, MailConfiguration.class})
 @WebAppConfiguration
 public class RestServiceFacadeTest {
 
@@ -25,5 +48,87 @@ public class RestServiceFacadeTest {
     @Autowired
     private DocumentService documentService;
 
+    @MockBean
+    private NotificationService notificationService;
 
+    private static final String userName = "TestUser";
+    private static final String localization = "France";
+    private static final String documentName = "test.txt";
+
+    @Before
+    public void setup() throws Exception {
+        DocumentServiceController documentServiceController = new DocumentServiceController();
+        ReflectionTestUtils.setField(documentServiceController, "documentService", documentService);
+        this.mockMvc = MockMvcBuilders.standaloneSetup(documentServiceController).build();
+    }
+
+    @After
+    public void testDown() {
+        deleteDirectory(new File(FileSystemDocumentDAOImpl.ROOT_DIRECTORY));
+    }
+
+    @Test
+    public void uploadFile_ShouldReturnMetadata() throws Exception {
+//        String userName = "TestUser";
+//        String localization = "France";
+        Date uploadDate = new Date();
+        final byte[] content = "Hello World".getBytes();
+        MockMultipartFile mockFile = new MockMultipartFile("file", documentName, "text/plain", content);
+
+        MetadataDocument metadataDocument = documentService.saveDocument(mockFile, userName, localization, uploadDate);
+
+        mockMvc.perform(fileUpload("/service/upload")
+                .file(mockFile)
+                .param("userName", userName)
+                .param("localization", localization)
+                .contentType(MediaType.APPLICATION_JSON)).andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.id").exists())
+                .andExpect(jsonPath("$.userName", is(metadataDocument.getUserName())))
+                .andExpect(jsonPath("$.documentName", is(metadataDocument.getDocumentName())))
+                .andExpect(jsonPath("$.localization", is(metadataDocument.getLocalization())))
+                .andExpect(jsonPath("$.date").exists());
+    }
+
+    @Test
+    public void findMetadataDocuments_SHouldReturnMetadata() throws Exception {
+
+        String userName = "TestUser";
+        String localization = "France";
+
+        List<MetadataDocument> metadataDocuments = documentService.findMetadataDocuments(userName, localization);
+
+        mockMvc.perform(get("/service/metadata")
+                .param("userName", userName)
+                .param("localization", localization)
+                .contentType(MediaType.APPLICATION_JSON)).andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$", hasSize(2)))
+                .andExpect(jsonPath("$[0].id").exists())
+                .andExpect(jsonPath("$[0].userName", is(userName)))
+                .andExpect(jsonPath("$[0].documentName", is(documentName)))
+                .andExpect(jsonPath("$[0].localization", is(localization)))
+                .andExpect(jsonPath("$[0].date").exists())
+                .andExpect(jsonPath("$[1].id").exists())
+                .andExpect(jsonPath("$[1].userName", is(userName)))
+                .andExpect(jsonPath("$[1].documentName", is(documentName)))
+                .andExpect(jsonPath("$[1].localization", is(localization)))
+                .andExpect(jsonPath("$[1].date").exists());
+    }
+
+    public static boolean deleteDirectory(File directory) {
+        if (directory.exists()) {
+            File[] files = directory.listFiles();
+            if (null != files) {
+                for (int i = 0; i < files.length; i++) {
+                    if (files[i].isDirectory()) {
+                        deleteDirectory(files[i]);
+                    } else {
+                        files[i].delete();
+                    }
+                }
+            }
+        }
+        return (directory.delete());
+    }
 }
